72cc7e6ff31b26fd5482af66d18cf005e5334e87
Sparsh Verma <81426512+sparshrex@users.noreply.github.com>
2023-07-05 12:24:44 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (96.00%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    
+    bool valid(int i,int j,int m,int n)
+    {
+        if(i>=0 && i<m && j<n && j>=0)return true;
+      return false;
+    }
+
+ 
+
+
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size(),n= board[0].size();
+         int x[]={0,0,-1,1,1,-1,1,-1};
+        int y[]={1,-1,0,0,1,-1,-1,1};
+       for(int i =0;i<m;i++)
+       {
+           for(int j =0;j<n;j++)
+           {
+               int live =0;
+            for(int index = 0;index<8;index++)
+            {
+
+            if(valid(i+x[index],j+y[index],m,n) && abs(board[i+x[index]][j+y[index]])==1){
+            live++;
+            }
+            
+            }
+
+            if(board[i][j]==1 && (live < 2 || live >3))
+            {
+                board[i][j]=-1;
+            }
+            if(board[i][j]==0 && live==3)
+            {
+                board[i][j]=2;
+            }
+           }
+       }
+
+       for(int i=0;i<m;i++)
+       {
+           for(int j =0;j<n;j++)
+           {
+               if(board[i][j]<=0)board[i][j]=0;
+               else
+               board[i][j] = 1;
+           }
+       }
+       
+       
+    }
+};
