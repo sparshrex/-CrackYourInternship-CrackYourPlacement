9ff8e4481ff8f5e673bace393d3238f281dd1467
Sparsh Verma <81426512+sparshrex@users.noreply.github.com>
2023-07-09 20:44:50 +0530

    Time: 4 ms (40.42%), Space: 6.4 MB (9.07%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<char>st;
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(!st.empty()&& s[i]==')' && st.top()=='(')
+            {
+                st.pop();
+            }
+            else if(!st.empty() && s[i]=='}' && st.top()=='{')
+            {
+                st.pop();
+            }
+            else if(!st.empty() && s[i]==']' && st.top()=='[')
+            {
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        if(st.size()==0)return true;
+        return false;
+    }
+};
